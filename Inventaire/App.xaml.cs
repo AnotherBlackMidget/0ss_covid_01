a628607f2d439d45ca7cbed6dc1e907a0bc21087
unknown <anotherblackmidget@gmail.com>
2020-04-06 19:26:04 -0400

    3.4 MVVM

--- Inventaire/App.xaml.cs
+++ Inventaire/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BillingManagement.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,15 @@ namespace Inventaire
     /// </summary>
     public partial class App : Application
     {
+        CustomerView _wnd;
+
+        public App()
+        {
+            CustomersViewModel vm = new CustomersViewModel();
+
+            _wnd = new CustomerView(vm);
+
+            _wnd.Show();
+        }
     }
 }
---
+++ Inventaire/View/CustomerView.xaml.cs
@@ -0,0 +1,53 @@
+﻿using BillingManagement.Models;
+using BillingManagement.Business;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using BillingManagement.UI.ViewModels;
+
+namespace Inventaire
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CustomerView : Window
+    {
+        
+        public CustomerView(CustomersViewModel vm)
+        {
+            InitializeComponent();
+
+            DataContext = vm;
+        }
+
+
+        
+        
+        private void CustomerNew_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            Customer temp = new Customer() { Name = "Undefined", LastName = "Undefined" };
+            Customers.Add(temp);
+            SelectedCustomer = temp;  
+            */
+        }
+
+        private void CustomerDelete_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            int currentIndex = Customers.IndexOf(SelectedCustomer);
+
+            if (currentIndex > 0)
+                currentIndex--;
+
+            Customers.Remove(SelectedCustomer);
+
+            lvCustomers.SelectedIndex = currentIndex;
+            */
+        }
+        
+    }
+}
