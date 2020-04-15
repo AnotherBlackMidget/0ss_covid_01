1af36492a6f179edac46158eacd66e8c3b5bb4b6
unknown <anotherblackmidget@gmail.com>
2020-04-15 18:43:51 -0400

    3.2 Models

---
+++ app_models/Invoice.cs
@@ -0,0 +1,100 @@
+﻿using app_models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BillingManagement.Models
+{
+    public class Invoice : INotifyPropertyChanged
+    {
+        private static List<bool> UsedCounter = new List<bool>();
+        private static object Lock = new object();
+
+        private int InvoiceID;
+        private DateTime CreationDateTime;
+        private Customer Customer;
+        private Double SubTotal
+        {
+            get => SubTotal;
+            set
+            {
+                FedTax = SubTotal * 0.05;
+                ProvTax = SubTotal * 0.09975;
+                Total = SubTotal + FedTax + ProvTax;
+                OnPropertyChanged();
+            }
+        }
+        private Double FedTax;
+        private Double ProvTax;
+        private Double Total;
+        
+
+        Invoice()
+        {
+            lock (Lock)
+            {
+                int nextIndex = GetAvailableIndex();
+                if (nextIndex == -1)
+                {
+                    nextIndex = UsedCounter.Count;
+                    UsedCounter.Add(true);
+                }
+
+                InvoiceID = nextIndex;
+            }
+
+            CreationDateTime = DateTime.Now;
+        }
+
+        Invoice(Customer cm)
+        {
+            lock (Lock)
+            {
+                int nextIndex = GetAvailableIndex();
+                if (nextIndex == -1)
+                {
+                    nextIndex = UsedCounter.Count;
+                    UsedCounter.Add(true);
+                }
+
+                InvoiceID = nextIndex;
+            }
+
+            CreationDateTime = DateTime.Now;
+            Customer = cm;
+        }
+
+        public void Dispose()
+        {
+            lock (Lock)
+            {
+                UsedCounter[InvoiceID] = false;
+            }
+        }
+
+
+        private int GetAvailableIndex()
+        {
+            for (int i = 0; i < UsedCounter.Count; i++)
+            {
+                if (UsedCounter[i] == false)
+                {
+                    return i;
+                }
+            }
+
+            // Nothing available.
+            return -1;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
